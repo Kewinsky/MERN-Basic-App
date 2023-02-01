26eb2601aee5347dd0e42f4f5d0e4ea8d7389be2
Kewin Tao Anh <kewin.taoanh@gmail.com>
2023-02-01 11:26:47 +0100

    add comments to controllers

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,11 +3,17 @@ const asyncHandler = require("express-async-handler");
 const Goal = require("../models/goalModel");
 const User = require("../models/userModel");
 
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
 const getGoals = asyncHandler(async (req, res) => {
   const goals = await Goal.find({ user: req.user.id });
   res.status(200).json({ goals });
 });
 
+// @desc    Set goal
+// @route   POST /api/goals
+// @access  Private
 const setGoals = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
@@ -22,6 +28,9 @@ const setGoals = asyncHandler(async (req, res) => {
   res.status(200).json(goal);
 });
 
+// @desc    Update goal
+// @route   PUT /api/goals/:id
+// @access  Private
 const updateGoals = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
 
@@ -51,6 +60,9 @@ const updateGoals = asyncHandler(async (req, res) => {
   res.status(200).json(updatedGoal);
 });
 
+// @desc    Delete goal
+// @route   DELETE /api/goals/:id
+// @access  Private
 const deleteGoals = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
 
